cdb225fdcf4f60631e40960f5783fb9052adbe02
sumitrastogi1998 <nanurastogi98>
2020-04-24 11:37:03 +0530

    Adding to git

--- CoronaProtection/server.js
+++ CoronaProtection/server.js
@@ -2,14 +2,14 @@ const express = require('express');
 const path = require('path');
  
 const app = express();
- 
+
 // Serve only the static files form the dist directory
 // Replace the '/dist/<to_your_project_name>'
-app.use(express.static(__dirname + '/dist/CORONAPROTECTION'));
+app.use(express.static(__dirname + '/dist/CoronaProtection'));
  
 app.get('*', function(req,res) {
   // Replace the '/dist/<to_your_project_name>/index.html'
-  res.sendFile(path.join(__dirname + '/dist/CORONAPROTECTION/index.html'));
+  res.sendFile(path.join(__dirname +'/dist/CoronaProtection/index.html'));
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
